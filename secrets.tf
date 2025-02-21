91200a2857e13e06dd683a8f06d6b95cc17e55ed
Pablo Menezes <pmenezes.aju@gmail.com>
2025-02-21 14:01:22 -0300

    Update module

--- secrets.tf
+++ secrets.tf
@@ -3,6 +3,6 @@ resource "aws_secretsmanager_secret" "redis_primary_endpoint" {
 }
 
 resource "aws_secretsmanager_secret_version" "redis_primary_endpoint" {
-  secret_id     = aws_secretsmanager_secret.redis.id
+  secret_id     = aws_secretsmanager_secret.redis_primary_endpoint.id
   secret_string = aws_elasticache_replication_group.main.primary_endpoint_address
 }
